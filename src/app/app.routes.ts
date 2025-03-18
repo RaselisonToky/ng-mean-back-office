7c20ad3988a74dc7285d2156306e40bab0cad9fa
ovoc01 <118508678+ovoc01@users.noreply.github.com>
2025-03-18 09:00:27 +0300

    fix: rename miss spelling inventory

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,29 @@
 import { Routes } from '@angular/router';
-import {AuthComponent} from './pages/auth/auth.component';
-import {LayoutComponent} from './layout/layout.component';
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
-import {SettingsComponent} from './pages/settings/settings.component';
-import {AuthGuard} from './pages/auth/services/auth-guards.service';
-import {ServiceComponent} from './pages/service/service.component';
-import {AppointmentComponent} from './pages/appointment/appointment.component';
-import {AppointmentFormComponent} from './pages/appointment/components/appointment-form/appointment-form.component';
-import { TransactionsComponent } from './pages/iventory/transactions/transactions.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { AuthGuard } from './pages/auth/services/auth-guards.service';
+import { ServiceComponent } from './pages/service/service.component';
+import { AppointmentComponent } from './pages/appointment/appointment.component';
+import { AppointmentFormComponent } from './pages/appointment/components/appointment-form/appointment-form.component';
+import { TransactionsComponent } from './pages/inventory/transactions/transactions.component';
+import {DeliveryComponent} from './pages/inventory/delivery/delivery.component'
 export const routes: Routes = [
-  {path: 'login', component: AuthComponent},
+  { path: 'login', component: AuthComponent },
   {
     path: '',
     component: LayoutComponent,
     children: [
-      { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] }},
-      { path: 'appointment', component: AppointmentComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] }},
-      { path: 'appointment-form', component: AppointmentFormComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] }},
+      { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
+      { path: 'appointment', component: AppointmentComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
+      { path: 'appointment-form', component: AppointmentFormComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
       { path: 'garage', component: SettingsComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
       { path: 'maintenance', component: SettingsComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
-      { path: 'service', component: ServiceComponent,  canActivate: [AuthGuard], data: { roles: ['ADMIN'] }},
-      {path:'inventory/transactions',component:TransactionsComponent, canActivate:[AuthGuard], data:{roles:['ADMIN']}},      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+      { path: 'service', component: ServiceComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
+      { path: 'inventory/transactions', component: TransactionsComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
+      { path: 'inventory/deliveries', component: TransactionsComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
     ]
   }
 ];
