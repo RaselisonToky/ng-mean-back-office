a75ae70018d41ec19fee6e7b9571f1b3a20f106a
ovoc01 <118508678+ovoc01@users.noreply.github.com>
2025-04-04 15:18:17 +0300

    feat

--- src/app/pages/inventory/supplier-order/service/supplier-order.service.ts
+++ src/app/pages/inventory/supplier-order/service/supplier-order.service.ts
@@ -6,13 +6,20 @@ import { environment } from '../../../../../environments/environments';
   providedIn: 'root'
 })
 export class SupplierOrderService {
-  private baseUrl = environment.apiUrl + '/inventory/supplier/order';
+  private baseUrl = environment.apiUrl + '/inventory/supplierOrder';
   constructor(private http: HttpClient) { }
   findAll() {
     return this.http.get(`${this.baseUrl}`);
   }
 
   search(query: any) {
+    // delete undefined properties from query
+    Object.keys(query).forEach(key => {
+      if (query[key] === undefined || query[key] === '') {
+        delete query[key];
+      }
+    });
+    // Convert date strings to ISO format if they are not empty
     return this.http.get(`${this.baseUrl}/search`, { params: query });
   }
 }
